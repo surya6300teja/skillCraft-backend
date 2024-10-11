562b9e25d5904d1bc82aa821f3725635de446812
suryatejarampuram <suryarampuram121@gmail.com>
2024-10-11 15:20:28 +0530

    port again3

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
+process.env.config();
 
 // Routes
 // app.get('/', (req, res) => {
